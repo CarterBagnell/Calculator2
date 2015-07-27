9a8f8e64309c55c6fe81a1e3492f07da76bcf1b3
CarterBagnell <bagnel63@students.rowan.edu>
2015-07-27 11:17:09 -0400

    Initial Commit

---
+++ Calculator2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator2
+//
+//  Created by Carter Bagnell on 7/27/15.
+//  Copyright (c) 2015 Carter Bagnell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
